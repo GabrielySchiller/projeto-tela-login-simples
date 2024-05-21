0eb43e95d573946c9233f23a18149c069174baa1
Gabriely Schiller <135662619+GabrielySchiller@users.noreply.github.com>
2024-05-21 17:21:50 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function clicou(){
+
+    var nom = document.getElementById("nome").value;
+    var sen= document.getElementById("senha").value;
+
+    
+    window.alert(`Bem vindo! ${nom}, sua ${sen} foi validada!`);
+}
+
+
+
+function cadastrou(){
+
+    var nome = document.getElementById("nome").value;
+    var senha = document.getElementById("senha").value;
+    
+
+    window.alert(`Bem vindo! ${nome}, sua senha ${senha} foi validada!`);
+}
